1ca92db0ee1f8cfe24d0c3c621ca89891a28b384
Gustavo Moraes <gustavomoraes.ufrj@gmail.com>
2024-03-27 10:06:25 +0000

    testloop

--- exercs/testloop.py
+++ exercs/testloop.py
@@ -16,9 +16,10 @@ else:
 
 
 num = 131
-for i in range(2, num/2):
+for i in range(2, num):
     if num % i == 0:
         print(f"{num} is not a prime number")
         break
 else:
     print(f"{num} is a prime number")
+
